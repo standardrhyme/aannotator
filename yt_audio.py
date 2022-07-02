4c939d2e7e014b56f7d1c0f2ee235053e3508487
standardrhyme <asherkang.s@gmail.com>
2022-07-02 11:48:01 -0400

    adding YT download features

--- yt_audio.py
+++ yt_audio.py
@@ -1,5 +1,18 @@
-                                import youtube_dl
+# https://www.youtube.com/watch?v=CB39pBFyUFs
+
+import youtube_dl
 
 
-def main():
-        print('hello')
+def get_mp3():
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url=input("Enter video URL: "), download=False
+    )
+
+    options = {
+        "format": "bestaudio/bset",
+        "keepvideo": False,
+        "outtmpL": f"{video_info['title']}.mp3",
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info["webpage_url"]])
